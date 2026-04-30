dcf1545da8374899ac6ab24605978c19d1ea3a07
Sutton Wilterdink <swilterdink1010@floridapoly.edu>
2026-04-30 01:58:11 -0400

    feat: added object screen parsing

--- src/parse_screen.py
+++ src/parse_screen.py
@@ -1,8 +1,53 @@
-import pyautogui as auto
+from typing import List
+import time
 import numpy as np
+import pyautogui as auto
+
+from board import Board
+from screen_location_data import OBJ_LOCS, COMP_LOCS
 
 from solver import Solver
 
-def capture_board():
+
+COLORS = {
+    "BLUE":     [  76, 140, 230 ],
+    "YELLOW":   [ 255, 179,  30 ],
+    "BROWN":    [ 140, 114,  76 ],
+    "WHITE":    [ 255, 255, 255 ],
+}
+
+
+def _color_diff(color: List[int], other: List[int])->int:
+    diff = 0
+    for i in range(0, 3):
+        diff += abs(int(other[i]) - int(color[i]))
+    return diff
+
+
+def _detect_obj(color: List[int])->int:
+    if _color_diff(color, COLORS["YELLOW"]) < 20:
+        return 1
+    if _color_diff(color, COLORS["BLUE"]) < 20:
+        return 2
+    return 0
+
+def capture_board()->Board:
     ss = auto.screenshot()
     img = np.array(ss)
+    
+    board = Board()
+    
+    for row in range(0, 6):
+        for col in range(0, 6):
+            board.grid[row][col] = _detect_obj(img[OBJ_LOCS[row][col][0]][OBJ_LOCS[row][col][1]])
+            
+    return board
+    
+
+def main():
+    time.sleep(1)
+    capture_board().print()
+    
+    
+if __name__ == "__main__":
+    main()
--- src/screen_location_data.py
+++ src/screen_location_data.py
@@ -1,3 +1,11 @@
+_obj_x = [
+     749, 
+     836, 
+     922,
+    1009,
+    1095,
+    1182,
+]
 _obj_y = [
      342,
      429,
@@ -6,17 +14,31 @@ _obj_y = [
      689,
      776,
 ]
-_obj_x = [
-     749, 
-     836, 
-     922,
-    1009,
-    1095,
-    1182.
+_comp_h_x = [
+     786,
+     873,
+     960,
+    1046,
+    1133,
+]
+_comp_h_y = [
+     335,
+     422,
+     508,
+     595,
+     682,
+     768,
 ]
 
 OBJ_LOCS = [[(0, 0)] * 6 for _ in range(0, 6)]
 for row in range(0, 6):
     for col in range(0, 6):
-        OBJ_LOCS[row][col] = (_obj_x[col], _obj_y[row])
+        OBJ_LOCS[row][col] = (_obj_y[row], _obj_x[col])
         
+COMP_LOCS = [
+    [[(0, 0)] * 5 for _ in range(0, 6)],
+    [[(0, 0)] * 6 for _ in range(0, 5)],
+]
+for row in range(0, 6):
+    for col in range(0, 5):
+        COMP_LOCS[0][row][col] = (_comp_h_y[row], _comp_h_x[col])
